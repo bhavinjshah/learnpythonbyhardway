e2dea1ac6a1fc100563359f37df4b9f4097eb2bb
bhavin Shah <bhavinjshah@gmail.com>
2017-05-21 02:49:08 -0400

    example 34

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "the animal at 1 is %s" %animals[0]
+print "the third (3rd) animal %s" %animals[2]
+print "the first (1st) animal  is %s" %animals[0]
+print "the animal at 3 is %s" %animals[2]
+print "the 5th animal is %s" % animals[4]
+print "the animal at 2 is %s" %animals[1]
+print "the 6th animal is %s"  %animals[5]
+print "the 4th animal is %s" %animals[3]
